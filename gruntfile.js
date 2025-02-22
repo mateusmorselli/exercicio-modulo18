aab5d5ea2f2b7d3f5d47d065a4da2f005a89d2ec
Mateus Morselli <matmorselli@gmail.com>
2025-02-22 01:17:37 -0300

    correcao do JS minificado

--- gruntfile.js
+++ gruntfile.js
@@ -62,7 +62,7 @@ module.exports = function(grunt) {
                         },
                         {
                             match: 'ENDERECO_DO_JS',
-                            replacement: './src/scripts/main.min.js'
+                            replacement: './scripts/main.min.js'
                         },
                     ]
                 },
